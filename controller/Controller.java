20fe96bbaff19500c1da3966297985964bd94824
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-07-05 11:19:36 +0100

    Add files via upload

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,9 +1,10 @@
 package com.codegym.task.task34.task3410.controller;
 
+import com.codegym.task.task34.task3410.model.Direction;
 import com.codegym.task.task34.task3410.model.Model;
 import com.codegym.task.task34.task3410.view.View;
 
-public class Controller {
+public class Controller implements EventListener {
     View view;
     Model model;
 
@@ -11,6 +12,26 @@ public class Controller {
         view = new View(this);
         model = new Model();
         view.init();
+    }
+
+    @Override
+    public void move(Direction direction) {
+
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void startNextLevel() {
+
+    }
+
+    @Override
+    public void levelCompleted(int level) {
+
     }
 
     public static void main(String[] args) {
