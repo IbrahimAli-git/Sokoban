3d1d88cc4b52cb3e7c3f3bf271add10cc8ca36d1
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-07-06 11:07:40 +0100

    Add files via upload

--- model/Model.java
+++ model/Model.java
@@ -2,12 +2,45 @@ package com.codegym.task.task34.task3410.model;
 
 import com.codegym.task.task34.task3410.controller.EventListener;
 
+import java.net.URISyntaxException;
+import java.nio.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
 public class Model {
     public static final int BOARD_CELL_SIZE = 20;
     protected EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    LevelLoader levelLoader;
 
+    public Model() {
+        try {
+            levelLoader = new LevelLoader(Paths.get(getClass().getResource("../res/levels.txt").toURI()));
+        } catch (URISyntaxException ignore) {
+
+        }
+    }
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects(){
+        return gameObjects;
+    }
+
+    public void restartLevel(int level){
+         gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart(){
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel(){
+        currentLevel++;
+        restartLevel(currentLevel);
     }
 }
