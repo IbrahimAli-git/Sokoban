76a2cc547911860a08e11bb8f04fba680e1ad45c
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-07-06 11:43:32 +0100

    Update Board.java

--- view/Board.java
+++ view/Board.java
@@ -1,10 +1,12 @@
 package com.codegym.task.task34.task3410.view;
 
 import com.codegym.task.task34.task3410.controller.EventListener;
+import com.codegym.task.task34.task3410.model.Direction;
 import com.codegym.task.task34.task3410.model.GameObject;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Board extends JPanel {
     View view;
@@ -12,6 +14,9 @@ public class Board extends JPanel {
 
     public Board(View view){
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     public void setEventListener(EventListener eventListener) {
@@ -25,6 +30,24 @@ public class Board extends JPanel {
 
         for (GameObject gameObject : view.getGameObjects().getAll()) {
             gameObject.draw(g);
+        }
+    }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            if (keyCode == KeyEvent.VK_LEFT){
+                eventListener.move(Direction.LEFT);
+            } else if (keyCode == KeyEvent.VK_RIGHT){
+                eventListener.move(Direction.RIGHT);
+            } else if (keyCode == KeyEvent.VK_UP){
+                eventListener.move(Direction.UP);
+            } else if (keyCode == KeyEvent.VK_DOWN){
+                eventListener.move(Direction.DOWN);
+            } else if (keyCode == KeyEvent.VK_R){
+                eventListener.restart();
+            }
         }
     }
 }
