4a6476a1eefa77fff4783a2dd987b906d2aff066
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-07-06 11:16:45 +0100

    Update Board.java

--- view/Board.java
+++ view/Board.java
@@ -1,6 +1,7 @@
 package com.codegym.task.task34.task3410.view;
 
 import com.codegym.task.task34.task3410.controller.EventListener;
+import com.codegym.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,7 +20,11 @@ public class Board extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
+        for (GameObject gameObject : view.getGameObjects().getAll()) {
+            gameObject.draw(g);
+        }
     }
-
 }
