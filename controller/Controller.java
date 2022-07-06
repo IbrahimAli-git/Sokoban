fbf84c950135e5facb1501e464a3b3ac45b3732d
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-07-06 11:16:11 +0100

    Update Controller.java

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,6 +1,7 @@
 package com.codegym.task.task34.task3410.controller;
 
 import com.codegym.task.task34.task3410.model.Direction;
+import com.codegym.task.task34.task3410.model.GameObjects;
 import com.codegym.task.task34.task3410.model.Model;
 import com.codegym.task.task34.task3410.view.View;
 
@@ -12,6 +13,7 @@ public class Controller implements EventListener {
         view = new View(this);
         model = new Model();
         view.init();
+        model.restart();
     }
 
     @Override
@@ -32,6 +34,10 @@ public class Controller implements EventListener {
     @Override
     public void levelCompleted(int level) {
 
+    }
+
+    public GameObjects getGameObjects() {
+        return model.getGameObjects();
     }
 
     public static void main(String[] args) {
