e704a720d0493199ca65164a43ba0e37521430d8
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-07-06 11:31:03 +0100

    Update Controller.java

--- controller/Controller.java
+++ controller/Controller.java
@@ -14,26 +14,31 @@ public class Controller implements EventListener {
         model = new Model();
         view.init();
         model.restart();
+        model.setEventListener(this);
+        view.setEventListener(this);
     }
 
     @Override
     public void move(Direction direction) {
-
+        model.move(direction);
+        view.update();
     }
 
     @Override
     public void restart() {
-
+        model.restart();
+        view.update();
     }
 
     @Override
     public void startNextLevel() {
-
+        model.startNextLevel();
+        view.update();
     }
 
     @Override
     public void levelCompleted(int level) {
-
+        view.completed(level);
     }
 
     public GameObjects getGameObjects() {
@@ -44,3 +49,4 @@ public class Controller implements EventListener {
         Controller controller = new Controller();
     }
 }
+
